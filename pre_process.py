66f0ce936e7816100c56b2cc6203cc259137ae73
ahmetalkilinc <alkilincahmet@gmail.com>
2020-05-26 22:02:37 +0300

    preprocess is added

---
+++ pre_process.py
@@ -0,0 +1,31 @@
+import nltk
+import re
+import string
+
+
+class Preprocess:
+
+    def __init__(self, sentence):
+        self.sentence = sentence
+        print("Preprocessing...")
+
+
+    def replace_nonAlphabetic(sentence):
+        sentence=[re.sub(r'[^a-z]',' ',text.lower()) for text in sentence]
+
+        return sentence
+
+    def remove_puctiaation(sentence):
+        sentence = sentence.translate(string.punctuation)
+
+    def remove_stopword(sentence):
+        stops = nltk.corpus.stopwords.words('english')
+        sentence = [[word for word in text if word not in stops] for text in sentence]
+
+        return sentence
+
+    def stemming(sentence):
+        porter = nltk.stem.porter.PorterStemmer()
+        sentence = [[porter.stem(word) for word in text] for text in sentence]
+
+        return sentence
