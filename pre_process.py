cbe4f24789cf6354848e1ac865a62cbb0fad3f78
ahmetalkilinc <alkilincahmet@gmail.com>
2020-05-28 16:05:24 +0300

    some method added and updated

--- pre_process.py
+++ pre_process.py
@@ -9,14 +9,25 @@ class Preprocess:
         self.sentence = sentence
         print("Preprocessing...")
 
-
     def replace_nonAlphabetic(sentence):
-        sentence=[re.sub(r'[^a-z]',' ',text.lower()) for text in sentence]
+        sentence = [re.sub(r'[^a-z]', ' ', text.lower()) for text in sentence]
+
+        return sentence
+
+    def replace_quotations(sentence):
+        sentence = re.sub("\n", " ", sentence)
+
+        sentence = re.sub("\"", "\"", sentence)
+        sentence = re.sub("''", "\"", sentence)
+        sentence = re.sub("``", "\"", sentence)
+        sentence = re.sub(" +", " ", sentence)
 
         return sentence
 
     def remove_puctiaation(sentence):
         sentence = sentence.translate(string.punctuation)
+
+        return sentence
 
     def remove_stopword(sentence):
         stops = nltk.corpus.stopwords.words('english')
